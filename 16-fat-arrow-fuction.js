5b67e971d367f8583671e6618f1db5502177576a
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2023-10-09 23:45:05 -0300

    Create example file for fat arrow function

---
+++ 16-fat-arrow-fuction.js
@@ -0,0 +1,14 @@
+// Definición de la función 'miFatArrowFunction' usando una fat arrow function (=>).
+// Las fat arrow functions fueron introducidas en ECMAScript 6 (ES6) como una sintaxis más concisa
+// para escribir funciones. Esta sintaxis es especialmente útil para funciones que contienen una sola expresión.
+// En este caso, 'miFatArrowFunction' es una función que recibe dos argumentos 'a' y 'b',
+// y retorna la suma de 'a' y 'b'.
+const miFatArrowFuction = (a, b) => a + b;
+
+// Llamada a la función 'miFatArrowFunction' con los argumentos 1 y 2.
+// La función retorna la suma de 1 y 2, que es 3, y este valor es asignado a la variable 'resultado'.
+const resultado = miFatArrowFuction(1, 2);
+
+// Impresión del resultado en la consola.
+// Se imprime "El resultado es 3" en la consola.
+console.log("El resultado es", resultado);
