5e701df539afa152ec9e19408c932ea0c503e24b
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2023-10-12 20:52:10 -0300

    Modify comments to use JSDoc format when explaining callback functions

--- 15-callbacks.js
+++ 15-callbacks.js
@@ -1,21 +1,28 @@
-// Definición de la función 'sumar'
-// Esta función recibe tres argumentos: 'a', 'b' y 'cb'.
-// 'a' y 'b' son números que serán sumados,
-// mientras que 'cb' es una función callback que será invocada con el resultado de la suma.
+/**
+ * Función 'sumar' que calcula la suma de dos números y luego invoca una función callback con el resultado.
+ *
+ * @param {number} a - El primer número a sumar.
+ * @param {number} b - El segundo número a sumar.
+ * @param {function} cb - La función callback que será invocada con el resultado de la suma.
+ */
 function sumar(a, b, cb) {
   const r = a + b; // Calcula la suma de 'a' y 'b'
   cb(r); // Invoca la función callback 'cb' pasando el resultado 'r' como argumento
 }
 
-// Definición de la función 'callback'
-// Esta función recibe un argumento 'result', que es el resultado de la suma.
-// Imprime el resultado en la consola.
+/**
+ * Función 'callback' que recibe un argumento y lo imprime en la consola junto con la cadena "resultado".
+ *
+ * @param {number} result - El resultado que será impreso en la consola.
+ */
 function callback(result) {
-  console.log("resultado", result);
+  console.log("resultado", result); // Imprime "resultado" y el valor de 'result' en la consola
 }
 
-// Llamada a la función 'sumar'
-// Se pasan los números 2 y 3 para ser sumados, y la función 'callback' para ser invocada con el resultado.
-// La función 'callback' será invocada dentro de 'sumar', y recibirá el resultado de la suma (5) como argumento,
-// lo que resultará en la salida: "resultado 5" en la consola.
-sumar(2, 3, callback);
+/**
+ * Llamada a la función 'sumar' pasando dos números y la función 'callback' como argumentos.
+ *
+ * La función 'callback' será invocada dentro de 'sumar' con el resultado de la suma,
+ * y imprimirá el mensaje "resultado" seguido del resultado en la consola.
+ */
+sumar(2, 3, callback); // Output: "resultado 5"
