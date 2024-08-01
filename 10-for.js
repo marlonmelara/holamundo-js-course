3a1915cbddb13e39da10292bcb1b32240b84ffdc
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2024-07-31 21:55:13 -0400

    Update comments

--- 10-for.js
+++ 10-for.js
@@ -3,10 +3,11 @@ for (let i = 0; i < 10; i++) {
   console.log(i);
 }
 
-/* const numbers = [1, 2, "Hola", 4, 5];
+// Acceder a elementos de un array usando "for"
+const numbers = [1, 2, "Hola", 4, 5];
 
 console.log(numbers[2]);
 
 for (let i = 0; i < numbers.length; i++) {
   console.log(numbers[i]);
-} */
+}
