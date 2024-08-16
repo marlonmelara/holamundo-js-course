8b836b403c1ce663ac282d800ea5e39554e7334a
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2024-08-16 16:51:45 -0400

    Update comments and create pseudocode file

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -26,20 +26,35 @@ const actualizaTodos = () => {
   localStorage.setItem("todos", todoString); // Almacena el string JSON en el localStorage.
 };
 
-// 0 Controlador de eventos que se ejecuta cuando se carga la ventana.
+// ## 1. Al cargar la página
+// Controlador de eventos que se ejecuta cuando se carga la ventana.
 window.onload = () => {
-  render();
-  // 1 Obtener referencia al formulario donde se ingresara las tareas pendientes
+  render(); // 1.1 Renderizar tareas guardadas
+
+  // ## 2. Cuando el usuario envía el formulario:
+  // 2.1 Obtener referencia al formulario donde se ingresarán las tareas pendientes
   const form = document.getElementById("todo-form");
 
-  // 2 Controlador de eventos que se dispara al enviar el formulario.
+  // 2.2 Asignar un controlador de eventos al formulario cuando se envía
   form.onsubmit = (e) => {
-    e.preventDefault(); // Evita que el formulario se envíe y la página se recargue.
-    const todo = document.getElementById("todo"); //Ref al elemento con el id "todo-form".
+    e.preventDefault(); // 2.2.1 Prevenir recarga de la página
+
+    // 2.2.2 Obtener el valor de la tarea ingresada
+    const todo = document.getElementById("todo"); // Ref al elemento con el id "todo" (<input type="text" id="todo" />)
     const todoText = todo.value; // Obtiene el valor del campo de entrada del formulario.
-    todo.value = ""; // Reinicia el campo de entrada del formulario.
+
+    todo.value = ""; // 2.2.3 Limpiar el campo de entrada del formulario.
+
+    // 2.2.4 Actualizar la lista de tareas
     todos.push(todoText); // Añade la nueva tarea al array 'todos'.
     actualizaTodos(); // Actualiza el almacenamiento local con la nueva lista de tareas.
     render(); // Vuelve a renderizar la lista de tareas para mostrar la nueva tarea.
   };
 };
+
+/** ¿Cuándo usar window.onload?
+Si necesitas asegurarte de que todos los recursos de la página, incluidos los archivos de imágenes y CSS, estén completamente cargados antes de ejecutar el código.*/
+
+/**
+ * .onsubmit es una propiedad que se usa para asignar un controlador de eventos que se ejecutará cuando un formulario sea enviado. Es decir, cuando el usuario hace clic en un botón de envío (generalmente un botón de tipo submit), o presiona Enter dentro de un campo de texto de un formulario, se dispara el evento submit, y el código asociado a .onsubmit se ejecuta.
+ */
---
+++ intro-dom/todo-list-pseudocodigo.md
@@ -0,0 +1,14 @@
+# ToDo List - Pseudocódigo
+
+## 1. Al cargar la página
+
+1.1 Renderizar tareas guardadas.
+
+## 2. Cuando el usuario envía el formulario
+
+- 2.1 Obtener referencia al formulario donde se ingresarán las tareas pendientes.
+- 2.2 Asignar un controlador de eventos al formulario cuando se envía.
+  - 2.2.1 Prevenir recarga de la página.
+  - 2.2.2 Obtener el valor de la tarea ingresada.
+  - 2.2.3 Limpiar el campo de entrada del formulario.
+  - 2.2.4 Actualizar la lista de tareas y renderizar nuevamente.
