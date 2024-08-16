30afff04db2fcd929ce8f6009ecb6760d811a031
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2024-08-16 17:03:18 -0400

    Update comments in main file and pseudocode file

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -1,35 +1,50 @@
-// 3 Inicializa el array 'todos' con los datos almacenados en localStorage o un array vacío si no hay nada almacenado.
+// ## 3. Inicialización de datos
+// 3.1 Inicializa el array 'todos' con los datos almacenados en localStorage o un array vacío si no hay nada almacenado.
 const todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-// 4 Función para renderizar la lista de tareas en la interfaz de usuario.
+// ## 4. Renderización de la lista de tareas
 const render = () => {
-  const todoList = document.getElementById("todo-list"); // Obtiene la referencia al elemento de lista de tareas en el DOM.
-  const todosTemplate = todos.map((t) => "<li>" + t + "</li>"); // Mapea el array de tareas a elementos <li> HTML y los almacena en el array 'todosTemplate'.
-  todoList.innerHTML = todosTemplate.join(""); // Convierte el array 'todosTemplate' a un string y lo establece como HTML interno de 'todoList'.
-  const elementos = document.querySelectorAll("#todo-list li"); // Obtiene todos los elementos <li> dentro de 'todoList'.
+  // 4.1 Obtener referencia a la lista de tareas en el DOM
+  const todoList = document.getElementById("todo-list");
+
+  // 4.2 Mapear el array de tareas a elementos <li> HTML
+  const todosTemplate = todos.map((t) => "<li>" + t + "</li>");
+
+  // 4.3 Actualizar el contenido del elemento 'todoList' en el DOM
+  todoList.innerHTML = todosTemplate.join("");
+
+  // 4.4 Obtener todos los elementos <li> dentro de 'todoList'
+  const elementos = document.querySelectorAll("#todo-list li");
+
+  // 4.5 Asignar un evento de eliminación a cada tarea
   elementos.forEach((elemento, i) => {
-    // Por cada elemento ejecutara una función
     elemento.addEventListener("click", () => {
-      // Elimina la tarea clickeada del DOM.
-      elemento.parentNode.removeChild(elemento); // Elimina la tarea del array 'todos'.
+      // 4.5.1 Eliminar la tarea del DOM
+      elemento.parentNode.removeChild(elemento);
+
+      // 4.5.2 Eliminar la tarea del array 'todos'
       todos.splice(i, 1);
-      // Actualiza el almacenamiento local y vuelve a renderizar la lista.
+
+      // 4.5.3 Actualizar el almacenamiento local y volver a renderizar la lista
       actualizaTodos(); // Actualiza el almacenamiento local.
       render(); // Vuelve a renderizar la lista de tareas.
     });
   });
 };
 
-// 5 Función para actualizar el almacenamiento local con la lista actual de tareas.
+// ## 5. Actualización del almacenamiento local
 const actualizaTodos = () => {
-  const todoString = JSON.stringify(todos); // Convierte el array 'todos' a un string JSON.
-  localStorage.setItem("todos", todoString); // Almacena el string JSON en el localStorage.
+  // 5.1 Convertir el array 'todos' a un string JSON
+  const todoString = JSON.stringify(todos);
+
+  // 5.2 Almacenar el string JSON en el localStorage
+  localStorage.setItem("todos", todoString);
 };
 
 // ## 1. Al cargar la página
-// Controlador de eventos que se ejecuta cuando se carga la ventana.
 window.onload = () => {
-  render(); // 1.1 Renderizar tareas guardadas
+  // 1.1 Renderizar tareas guardadas
+  render();
 
   // ## 2. Cuando el usuario envía el formulario:
   // 2.1 Obtener referencia al formulario donde se ingresarán las tareas pendientes
