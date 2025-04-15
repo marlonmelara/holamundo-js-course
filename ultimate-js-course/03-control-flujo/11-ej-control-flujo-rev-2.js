7b2838880efe2351975bc72995cab2a8bc70ccfb
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2025-04-15 15:33:31 -0400

    Add basic check for strength password

--- ultimate-js-course/03-control-flujo/11-ej-control-flujo-rev-2.js
+++ ultimate-js-course/03-control-flujo/11-ej-control-flujo-rev-2.js
@@ -19,3 +19,44 @@
   dependiendo de si la longitud de la contraseña es menor, igual o mayor
   que la longitud mínima definida.
 */
+
+// Usando IF (control de flujo)
+let password = "clave";
+const MIN_CHARACTERS = 12;
+
+if (password.length >= MIN_CHARACTERS) {
+  console.log("Es una contraseña segura.");
+} else {
+  console.log("Necesitas crear una contraseña con al menos 12 caracteres.");
+}
+
+// Usando función clásica
+const checkPasswordStrength = function (password) {
+  if (password.length >= MIN_CHARACTERS) {
+    return "Es una contraseña segura.";
+  } else {
+    return "Necesitas crear una contraseña con al menos 12 caracteres.";
+  }
+};
+
+console.log(checkPasswordStrength("holajavascript"));
+
+// Usando Arrow Fuction
+const checkPassword = (password) => {
+  if (password.length >= MIN_CHARACTERS) {
+    return "Es una contraseña segura.";
+  } else {
+    return "Necesitas crear una contraseña con al menos 12 caracteres.";
+  }
+};
+
+console.log(checkPassword("hola123456678"));
+
+// Usando operadores ternarios
+const checkPasswordStrengthMessage = (password) => {
+  return password.length >= MIN_CHARACTERS
+    ? "Es una contraseña segura."
+    : "Necesitas crear una contraseña con al menos 12 caracteres.";
+};
+
+console.log(checkPasswordStrengthMessage("123456")); // Resultado dinámico
